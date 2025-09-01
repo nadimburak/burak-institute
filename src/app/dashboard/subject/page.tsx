09f0d8f729e78b0436abefd4f0fe934351bcc19e
shad khan <shadkhan78667@gmail.com>
2025-09-01 16:07:32 +0530

    changes

---
+++ src/app/dashboard/subject/page.tsx
@@ -0,0 +1,12 @@
+import CourseTypeList from '@/components/course/courseTypes/list'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <SubjectList />
+        </>
+    )
+}
+
+export default page
