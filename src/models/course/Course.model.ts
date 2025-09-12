4d47a103c2e962f1a01915340cf587f583933b7a
RehanSheikh08 <RehanSheikh111222@gmail.com>
2025-09-12 15:54:48 +0530

    changes

--- src/models/course/Course.model.ts
+++ src/models/course/Course.model.ts
@@ -6,11 +6,11 @@ export interface ICourse extends Document {
     name: string;
     image: string;
     description?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
-const CourseSchema: Schema<ICourse> = new Schema(
+const CourseSchema = new Schema<ICourse>(
     {
         subject: {
             type: Schema.Types.ObjectId,
@@ -33,7 +33,6 @@ const CourseSchema: Schema<ICourse> = new Schema(
         },
         description: {
             type: String,
-            required: false,
             trim: true,
         },
     },
